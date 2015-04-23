43bf3900bc8331a559fdb64debbcbc8e5962d00d
DarzuL <guillaumebourderye@gmail.com>
2015-04-23 14:53:16 +0200

    TP1 - Modify text from Java
    Get TextView and modify its text from Java

--- app/src/main/java/com/example/evaneos/tp/fragment/MyFragment.java
+++ app/src/main/java/com/example/evaneos/tp/fragment/MyFragment.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.evaneos.tp.R;
 
 public class MyFragment extends Fragment {
+
+    TextView mTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_layout,container, false);
+        View v = inflater.inflate(R.layout.fragment_layout,container, false);
+        mTextView = (TextView) v.findViewById(R.id.my_text_view);
+        mTextView.setText(getString(R.string.hello_world3));
+        return v;
     }
 }
