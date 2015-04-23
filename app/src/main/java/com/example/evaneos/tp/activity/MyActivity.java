0de601574d87579ebaaeb345cb287d3a6ecaad44
DarzuL <guillaumebourderye@gmail.com>
2015-04-23 14:34:10 +0200

    TP1 - Activity Hello World
    Create java class which extends FragmentActivity
    Create layout for the activity
    Add the Activity into the Manifest.xml

---
+++ app/src/main/java/com/example/evaneos/tp/activity/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.evaneos.tp.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.evaneos.tp.R;
+
+public class MyActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
