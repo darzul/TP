4125e0d22e26fd9abda644e4d931b97ce49eb55a
DarzuL <guillaumebourderye@gmail.com>
2015-05-06 16:20:08 +0200

    A text list !
    Format class
    Rename parameters to be more explicit
    Implement onBindViewHolder
    Add refreshView(String txt) method to refresh the content of a
    ViewHolder (A view holder is re-used to display other data from the
    list, this is for performence interest)

--- app/src/main/java/com/example/evaneos/tp/adapter/MyAdapter.java
+++ app/src/main/java/com/example/evaneos/tp/adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.evaneos.tp.R;
 
@@ -14,7 +15,7 @@ import com.example.evaneos.tp.R;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private Context context;
-    private final String [] data;
+    private final String[] data;
 
     public MyAdapter(Context context, String[] data) {
         this.context = context;
@@ -22,14 +23,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.item_layout, viewGroup, false);
         return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
-
+    public void onBindViewHolder(MyViewHolder myViewHolder, int position) {
+        myViewHolder.refreshView(data[position]);
     }
 
     @Override
@@ -39,8 +40,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView tv;
+
         public MyViewHolder(View itemView) {
             super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.text);
+        }
+
+        public void refreshView(String s) {
+            tv.setText(s);
         }
     }
 }
