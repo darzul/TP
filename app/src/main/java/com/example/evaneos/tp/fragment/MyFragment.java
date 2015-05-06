8caeef3b4e09987a0db271cdf0fe4e605da06445
DarzuL <guillaumebourderye@gmail.com>
2015-05-06 15:50:01 +0200

    Get RecyclerView in your Fragment class
    Remove your old TextView object
    Replace it by a RecyclerView and get it from the inflated xml layout

--- app/src/main/java/com/example/evaneos/tp/fragment/MyFragment.java
+++ app/src/main/java/com/example/evaneos/tp/fragment/MyFragment.java
@@ -3,22 +3,21 @@ package com.example.evaneos.tp.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.evaneos.tp.R;
 
 public class MyFragment extends Fragment {
 
-    TextView mTextView;
+    RecyclerView mList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_layout,container, false);
-        mTextView = (TextView) v.findViewById(R.id.my_text_view);
-        mTextView.setText(getString(R.string.hello_world3));
+        View v = inflater.inflate(R.layout.fragment_layout, container, false);
+        mList = (RecyclerView) v.findViewById(R.id.list);
         return v;
     }
 }
